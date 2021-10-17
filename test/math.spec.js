db3e87bda939958030e3178fda9ad77a7bbe8f5c
Jardel <jardel1101@gmail.com>
2021-10-17 19:20:18 -0300

    Usando Chai para fazer assert nos testes

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,4 +1,5 @@
-const assert = require('assert');
+const expect = require('chai').expect;
+
 const Math = require('../src/math.js');
 
 describe('Math class', function () {
@@ -7,19 +8,39 @@ describe('Math class', function () {
   })
 
   it('Sum two numbers', function () {
-    assert.equal(math.sum(5, 5), 10);
+    expect(math.sum(5, 5)).to.equal(10);
   });
 
   it('Sum two numbers async', function (done) {
     this.timeout(3000)
 
     math.asyncSum(5, 5, value => {
-      assert.equal(value, 10);
+      expect(value).to.equal(10);
       done();
     })
   });
 
   it('Multiply two numbers', function() {
-    assert.equal(math.multiply(5, 5), 25);
+    expect(math.multiply(5, 5)).to.equal(25);
   })
+
+  it('Assert an object example', function () {
+    const userObj = { name: 'Jardel Bordignon' }
+
+    expect(userObj)
+      .to.have.property('name')
+      .equal('Jardel Bordignon');
+  });
+
+  it('Assert two objects example', function () {
+    const userObj = { name: 'Jardel Bordignon' }
+    const userObj2 = { name: 'Jardel Bordignon' }
+
+    // Comparando dois objetos diretamente mesmo que sejam iguais
+    // vai dar erro pois será comparada a referencia de cada um
+    // expect(userObj).to.equal(userObj2); //  refs não são iguais
+
+    // usando .to.deep.equal faz uma comparação profunda dos objetos
+    expect(userObj).to.deep.equal(userObj2); // params são iguais  
+  });
 });
